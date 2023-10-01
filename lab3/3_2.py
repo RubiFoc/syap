c22a7c51b010a9ef738f5de8655b55d25e00f085
RubiFoc <zhenya20057@mail.ru>
2023-10-01 15:40:14 +0400

    лаба 3 задание 2

---
+++ lab3/3_2.py
@@ -0,0 +1,26 @@
+# Имеется текстовый файл «Результаты соревнований», строка
+# которого содержит в себе информацию: фамилия спортсмена, результат.
+# Вывести на экран информацию о том, кто занял первое, второе и третье
+# места.
+# Пример файла:
+# Иванов 2
+# Петров 5– 3 балла
+
+with open('Результаты соревнований.txt', 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+
+results = []
+
+for line in lines:
+    parts = line.strip().split()
+    if len(parts) == 2 and 0 < int(parts[1]) < 4:
+        name, result = parts[0], int(parts[1])
+        results.append((name, result))
+
+sorted_results = sorted(results, key=lambda x: x[1])
+
+# Выводим информацию о первых трех местах
+print("Результаты соревнований:")
+for i, (name, result) in enumerate(sorted_results):
+    print(f"{result} место: {name}")
+
