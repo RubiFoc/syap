7b756ee94d62ca1696d0b505f60807c73e5cd55f
RubiFoc <zhenya20057@mail.ru>
2023-10-01 15:56:34 +0400

    лаба 3 задание 4

---
+++ lab3/3_4.py
@@ -0,0 +1,54 @@
+# 4. Создать вручную и заполнить несколькими строками текстовый
+# файл, в котором каждая строка будет содержать данные о фирме: название,
+# форма собственности, выручка, издержки.
+# Пример строки файла: firm_1 ООО 10000 5000.
+# Необходимо построчно прочитать файл, вычислить прибыль каждой
+# компании, а также среднюю прибыль. Если фирма получила убытки, в расчёт
+# средней прибыли её не включать.
+# Далее реализовать список. Он должен содержать словарь с фирмами и
+# их прибылями, а также словарь со средней прибылью. Если фирма получила
+# убытки, также добавить её в словарь (со значением убытков).
+# Пример списка: [{“firm_1”: 5000, “firm_2”: 3000, “firm_3”: 1000},
+# {“average_profit”: 2000}].
+# Итоговый список сохранить в виде json-объекта в соответствующий
+# файл.
+# Пример json-объекта:
+# [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit":
+# 2000}]
+# Подсказка: использовать менеджер контекста
+
+import json
+
+
+firm_list = []
+total_profit = 0
+firm_count = 0
+
+with open('firm_data.txt', 'r', encoding="utf-8") as file:
+    # Считываем строки из файла
+    lines = file.readlines()
+
+for line in lines:
+    parts = line.strip().split()
+    if len(parts) == 4:
+        name, ownership, revenue, expenses = parts
+        revenue = float(revenue)
+        expenses = float(expenses)
+        profit = revenue - expenses
+
+        if profit > 0:
+            firm_list.append({name: profit})
+            total_profit += profit
+            firm_count += 1
+
+if firm_count > 0:
+    average_profit = total_profit / firm_count
+else:
+    average_profit = 0
+
+result_list = [firm_list, {"average_profit": average_profit}]
+
+with open('firm_data.json', 'w') as json_file:
+    json.dump(result_list, json_file)
+
+print("Данные сохранены в файл firm_data.json")
